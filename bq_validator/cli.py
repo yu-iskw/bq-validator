76604bb636a737078f65a6331a72ef09758e4ad6
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2024-05-02 09:40:12 +0900

    Parallelize the validations
    Signed-off-by: Yu Ishikawa <yu-iskw@users.noreply.github.com>

--- bq_validator/cli.py
+++ bq_validator/cli.py
@@ -12,8 +12,8 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import concurrent.futures
 import json
-import sys
 from typing import Optional
 
 import click
@@ -28,27 +28,40 @@ click_completion.init()
 
 
 @click.command()
-@click.argument('path', type=click.Path(exists=True))
-@click.option("--quota_project",
-              type=str,
-              required=False,
-              help="BigQuery client project ID")
-@click.option("--client_project",
-              type=str,
-              required=False,
-              help="BigQuery client project ID")
-@click.option("--client_location",
-              type=str,
-              required=False,
-              help="BigQuery client location")
-@click.option("--impersonate_service_account",
-              type=str,
-              required=False,
-              help="Impersonate service account email")
+@click.argument("path", type=click.Path(exists=True))
+@click.option(
+    "--quota_project", type=str, required=False, help="BigQuery client project ID"
+)
+@click.option(
+    "--client_project", type=str, required=False, help="BigQuery client project ID"
+)
+@click.option(
+    "--client_location", type=str, required=False, help="BigQuery client location"
+)
+@click.option(
+    "--impersonate_service_account",
+    type=str,
+    required=False,
+    help="Impersonate service account email",
+)
+@click.option(
+    "--num_parallels",
+    type=int,
+    required=False,
+    default=1,
+    help="Number of parallel query validations",
+)
+@click.option("--verbose", is_flag=True, help="Enable verbose output")
 @click.version_option(version=bq_validator.__version__)
-def main(path: str, quota_project: Optional[str], client_project: Optional[str],
-         client_location: Optional[str],
-         impersonate_service_account: Optional[str]):
+def main(
+    path: str,
+    quota_project: Optional[str],
+    client_project: Optional[str],
+    client_location: Optional[str],
+    impersonate_service_account: Optional[str],
+    num_parallels: Optional[int] = 1,
+    verbose: Optional[bool] = False,
+):
     """Validate BigQuery queries
 
     PATH is either of a SQL file path or a directory.
@@ -59,19 +72,34 @@ def main(path: str, quota_project: Optional[str], client_project: Optional[str],
         client_project_id=client_project,
         quota_project_id=quota_project,
         location=client_location,
-        impersonate_service_account=impersonate_service_account)
-    # Validate queries
+        impersonate_service_account=impersonate_service_account,
+    )
+    # Validate queries in parallel
     sql_files = get_sql_files(path=path)
     errors = {}
-    for query_file in sql_files:
-        query = read_file(path=query_file)
-        is_valid, error_message = validate_query(client=client, query=query)
-        if is_valid is False:
-            errors[query_file] = {
-                "query": query,
-                "error": error_message,
-            }
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_parallels) as executor:
+        futures = {
+            executor.submit(validate_and_collect_errors, client, sql_files, verbose)
+            for sql_files in sql_files
+        }
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            if result:
+                query_file, error_details = result
+                errors[query_file] = error_details
     # Show errors
     if len(errors) > 0:
         click.echo(json.dumps(errors, indent=2))
-        sys.exit(1)
+
+
+def validate_and_collect_errors(client, query_file, verbose: Optional[bool] = False):
+    """Validate a query and collect errors if any"""
+    if verbose:
+        click.echo(f"Validating {query_file}")
+    query = read_file(path=query_file)
+    is_valid, error_message = validate_query(client=client, query=query)
+    if not is_valid:
+        if verbose:
+            click.echo(f"Error: {error_message}")
+        return query_file, {"query": query, "error": error_message}
+    return None
